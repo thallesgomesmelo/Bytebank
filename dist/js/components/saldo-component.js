e07dfac971d015e754e8196bc9fa8a2ae94cac3c
thalles.melo <thalles.melo@mestria.com.br>
2024-03-12 09:22:32 -0300

    Melhorou a modularização.

--- dist/js/components/saldo-component.js
+++ dist/js/components/saldo-component.js
@@ -1,19 +1,20 @@
 import { formatarData, formatarMoeda } from "../utils/formatters.js";
 import { FormatoData } from "../types/FormatoData.js";
+import Conta from "../types/Conta.js";
 const elementSaldo = document.querySelector(".saldo-valor .valor");
 const elementDataAcesso = document.querySelector(".block-saldo time");
-let saldo = 3000;
 if (elementDataAcesso) {
-    const dataAcesso = new Date();
-    elementDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_SEMANA_DIA_MES_ANO);
+    elementDataAcesso.textContent = formatarData(Conta.getDataAcesso(), FormatoData.DIA_SEMANA_DIA_MES_ANO);
 }
-export function getSaldo() {
-    return saldo;
-}
-atualizarSaldo(saldo);
-export function atualizarSaldo(novoSaldo) {
-    saldo = novoSaldo;
+renderizarSaldo();
+function renderizarSaldo() {
     if (elementSaldo) {
-        elementSaldo.textContent = formatarMoeda(saldo);
+        elementSaldo.textContent = formatarMoeda(Conta.getSaldo());
     }
 }
+const SaldoComponent = {
+    atualizar() {
+        renderizarSaldo();
+    }
+};
+export default SaldoComponent;
--- src/components/saldo-component.ts
+++ src/components/saldo-component.ts
@@ -1,28 +1,28 @@
 import { formatarData, formatarMoeda } from "../utils/formatters.js";
 import { FormatoData } from "../types/FormatoData.js";
+import Conta from "../types/Conta.js";
 
 const elementSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
 const elementDataAcesso = document.querySelector(".block-saldo time") as HTMLElement;
 
-let saldo: number = 3000;
-
 if (elementDataAcesso) {
-  const dataAcesso: Date = new Date();
   elementDataAcesso.textContent = formatarData(
-    dataAcesso,
+    Conta.getDataAcesso(),
     FormatoData.DIA_SEMANA_DIA_MES_ANO
   );
 }
 
-export function getSaldo(): number {
-  return saldo;
-}
-
-atualizarSaldo(saldo);
-export function atualizarSaldo(novoSaldo: number): void {
-  saldo = novoSaldo;
-
+renderizarSaldo();
+function renderizarSaldo(): void {
   if (elementSaldo) {
-    elementSaldo.textContent = formatarMoeda(saldo);
+    elementSaldo.textContent = formatarMoeda(Conta.getSaldo());
   }
 }
+
+const SaldoComponent = {
+  atualizar() {
+    renderizarSaldo();
+  }
+};
+
+export default SaldoComponent;
